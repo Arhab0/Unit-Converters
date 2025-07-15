ea9eac52567f97f6dd68f7deb2e09c1ed10ecca2
Arhab0 <arhabumer2@gmail.com>
2025-07-15 16:14:38 +0500

    at admin panel footer removed

--- src/app/utils/components/DelayedFooter/DelayedFooter.tsx
+++ src/app/utils/components/DelayedFooter/DelayedFooter.tsx
@@ -1,11 +1,15 @@
 "use client";
 import { useEffect, useState } from "react";
 import Footer from "../Footer/Footer";
+import { usePathname } from "next/navigation";
 
 export default function DelayedFooter() {
+  const pathname = usePathname()
+  const pathSegments = pathname?.split("/") || [];
+  const isAdmin = pathSegments[1] !== "Admin";
   const [mounted, setMounted] = useState(false);
   setTimeout(() => {
     setMounted(true);
   }, 2000);
-  return mounted ? <Footer /> : null;
+  return mounted && isAdmin ? <Footer /> : null;
 }
